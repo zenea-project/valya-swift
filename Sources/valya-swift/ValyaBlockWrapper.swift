35475c806b779cb88c9c3a6e2a3e410905ab0f56
glasfisch3000 <jakob@danckwerts.net>
2024-02-18 11:30:52 +0100

    added debug prints

--- Sources/valya-swift/ValyaBlockWrapper.swift
+++ Sources/valya-swift/ValyaBlockWrapper.swift
@@ -30,18 +30,22 @@ public struct ValyaBlockWrapper: BlockStorageWrapper {
         let valya = Valya(self.version)
         
         var blocks: [Block.ID] = []
-        for block in valya.chunkData(content) {
+        let chunks = valya.chunkData(content)
+        print("valya put: \(chunks) (\(content))")
+        for block in chunks {
+            print("  \(block) -> ", terminator: "")
             switch await source.putBlock(content: block.content) {
-            case .success(block.id), .failure(.exists): blocks.append(block.id)
-            case .success(_): return .failure(.unable)
-            case .failure(let error): return .failure(error)
+            case .success(block.id), .failure(.exists): print("success"); blocks.append(block.id)
+            case .success(_): print("idk"); return .failure(.unable)
+            case .failure(let error): print("error: \(error)"); return .failure(error)
             }
         }
         
         switch valya.compress(blocks) {
-        case .error: return .failure(.unable)
-        case .empty: return await source.putBlock(content: Data())
+        case .error: print("compress: error"); return .failure(.unable)
+        case .empty: print("compress: empty"); return await source.putBlock(content: Data())
         case .success(let main, additional: let additional):
+            print("compress: \(main) + \(additional)")
             for block in additional {
                 switch await source.putBlock(content: block.content) {
                 case .success(block.id), .failure(.exists): break
